80f5719c96a8c4664a1d6f82c222d614bf511cbd
oliverisernmercado-bit <oliverisernmercado@gmail.com>
2026-06-02 15:33:45 +0200

    He añadido comentarios a los metodos

--- Palindromo.java
+++ Palindromo.java
@@ -1,4 +1,4 @@
-package palindromo;
+package palindromos;
 
 import java.util.Scanner;
 
@@ -11,32 +11,41 @@ public class Palindromo {
 		System.out.println("Introduce un string para comprobar si es un palíndromo o no:\n> ");
 		String texto = limpiarTexto(entrada.nextLine());
 
+		// Comprobamos si el texto introducido es palindromo con el metodo esPalindromo
 		if (esPalindromo(texto)) {
 			System.out.println("¡Sí es un palíndromo!");
-			
+
 		} else {
 			System.out.println("No es un palíndromo.");
 		}
 
 	}
-
+	
+	// Metodo que limpia el texto y lo pasa a minusculas para que la comprobacion no de problemas.
+	
 	static String limpiarTexto(String texto) {
+
+		// Comprobamos que el texto introducido no sea null.
 		if (texto == null) {
 			return "";
 		}
 
-		// El regex elimina cualquier caracter que no sea específicamente una letra
 		return texto.toLowerCase().replaceAll("\\P{L}+", "");
 	}
+
+	// Metodo que guarda en una variable el texto introducido pero al reves y
+	// lo compara.
 
 	static boolean esPalindromo(String texto) {
 
 		String textoAlReves = "";
 
+		// Guardamos el texto introducido al reves en la variable textoAlReves.
 		for (int i = texto.length() - 1; i >= 0; i--) {
 			textoAlReves += texto.charAt(i);
 		}
 
+		// comprobamos si son iguales o no, si lo son devuelve true, si no devuelve false.
 		return texto.equals(textoAlReves);
 	}
 }
